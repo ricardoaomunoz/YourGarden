0c6efc2b803bc95e7e60cc2175c1b30f3edebf16
Ricardo <raosoriom.lotopunto@gmail.com>
2020-05-13 20:12:45 -0500

    logica inversa prender led

--- app_indoor/services/gpio_control.py
+++ app_indoor/services/gpio_control.py
@@ -4,9 +4,9 @@ from gpiozero import LED, Button
 class Gpio_controller():
     def __init__(self):
         self.LIGHT_PIN = LED(17)
-        self.LIGHT_PIN.off()
-    def turn_on(self):
         self.LIGHT_PIN.on()
-    def turn_off(self):
+    def turn_on(self):
         self.LIGHT_PIN.off()
+    def turn_off(self):
+        self.LIGHT_PIN.on()
     
