a1fdd02dd304f769f35e9817ed84e8b26cf146f1
Ricardo <raosoriom.lotopunto@gmail.com>
2020-05-14 18:59:01 -0500

    adding broadcast 1.2

--- app_indoor/webstreaming.py
+++ app_indoor/webstreaming.py
@@ -63,7 +63,7 @@ def video_streaming():
 @socketio.on('connect')
 def test_connect():
 	print("CONECTED!!!!!!!")
-	emit('my_response', {'data': 'Connected', 'count': 0, 'light_state': LIGHT_STATE})
+	emit('my_response', {'data': 'Connected', 'count': 0})
 	# emit('light_status', {'light_state': LIGHT_STATE})
 
 @socketio.on('disconnect')
@@ -74,7 +74,7 @@ def test_disconnect():
 def test_message(message):
 	# USERS += 1
 	print("Start new Session!!")
-	session['receive_count'] = session.get('receive_count', 0) + 1
+	# session['receive_count'] = session.get('receive_count', 0) + 1
 	emit('light_status', {'light_state': LIGHT_STATE})
 
 
